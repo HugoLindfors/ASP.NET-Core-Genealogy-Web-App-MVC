3239e0a60f545468647d770033b44466ac6c4fe1
hugolindfors2 <hugo.lindfors2@elev.ga.ntig.se>
2022-11-19 15:58:04 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Genealogy_Web_App__MVC_.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
